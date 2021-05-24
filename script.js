231762128bd233797472825491c46a21d4cd1b21
MaganaOscar <oscmag13@gmail.com>
2021-05-23 17:16:09 -0700

    added connections changes

---
+++ script.js
@@ -0,0 +1,17 @@
+function changeName() {
+    var name = document.getElementById("username");
+    name.innerText = "any other name";
+}
+
+function accept (elem) {
+    elem.parentElement.remove();
+
+    var currentRequests = document.getElementById("requests");
+    var request = parseInt(currentRequests.innerText);
+    currentRequests.innerText = --request;
+
+    var currentConnections = document.getElementById("connections");
+    var connections = parseInt(currentConnections.innerText);
+    currentConnections.innerText = (connections + 1) + "+";
+    console.log(connections);
+}
